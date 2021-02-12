45e8e25f0569af3b445c03cdec46ef95a82ef6ac
udithdilshan <45947626+udithdilshan@users.noreply.github.com>
2021-02-12 21:12:46 +0530

    Multiplication table

--- new.py
+++ new.py
@@ -16,4 +16,11 @@ while(n<8):
   print(n)
   n = n +1
   
-  
+# Multiplication table
+
+# To take input from the user
+num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
